722b9883e75085ed2477e6e7b9a650ab8eb7efd2
sawada <sawadasuiren@gmail.com>
2017-09-15 22:37:24 +0900

    Add reducers

--- src/AppBootstrap.js
+++ src/AppBootstrap.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
+import reducer from './reducers/index';
+
+let store = createStore(reducer);
 
 render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('Main')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+const app = (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+};
+export default app;
