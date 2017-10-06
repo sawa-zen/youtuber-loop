1192907f54828ea4454e7b241ea8fae6a0655fef
SAWADA Takayoshi <sawadasuiren@gmail.com>
2017-10-06 14:55:57 +0900

    Add Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import CSS from './App.css';
 import Header from './header/Header';
+import Footer from './footer/Footer';
 
 const App = ({ children }) => (
   <div className={CSS.App}>
     <Header className={CSS.AppHeader} />
-    <div className={CSS.AppBackground}>
-    </div>
-    <div className={CSS.AppContent}>
-      {children}
+    <div className={CSS.AppBody}>
+      <div className={CSS.AppContent}>
+        {children}
+      </div>
+      <Footer />
     </div>
   </div>
 );
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import CSS from './Footer.css';
+
+const Footer = () => (
+  <div className={CSS.Footer}>
+  </div>
+);
+export default Footer;
