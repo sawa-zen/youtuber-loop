671377904d7df0011d5fe4d9115b0ee271e2dce9
SAWADA Takayoshi <sawadasuiren@gmail.com>
2017-10-15 18:38:49 +0900

    Fix ChannelIcon

--- src/components/channelIcon/ChannelIcon.js
+++ src/components/channelIcon/ChannelIcon.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import classNames from 'classnames';
 import CSS from './ChannelIcon.css';
 
-const ChannelIcon = () => (
-  <div className={CSS.ChannelIcon}>
+const ChannelIcon = ({ className }) => (
+  <div className={classNames(CSS.ChannelIcon, className)}>
     <img src="https://avatars1.githubusercontent.com/u/3971271?v=4&s=460" alt="" />
   </div>
 );
